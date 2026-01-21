b7f6dd14af8912c665fd57087eb0f18a178536ab
Dxthstr0k3 <sladeindustries@gmail.com>
2026-01-21 17:39:36 +0200

    Complete Redesign
    - Green & Black
    - Glass Navbar
    - Added 4 new sections (About, Services, Contact Me, Footer)
    - Fixed all visual and code errors
    - Made responsive for smaller devices
    - Added JS file
    - Added functionality to Menu (Expand/Collapse)

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bi-x-lg');
+    navbar.classList.toggle('active');
+}
