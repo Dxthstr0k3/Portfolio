38b8557ce36d51d8819c6ca835e187980b63eeb2
Dxthstr0k3 <sladeindustries@gmail.com>
2026-02-18 23:59:02 +0200

    Minor Tweaks
    Update to CSS and JS

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ const resumeModal = document.getElementById('resumeModal');
 const closeModal = document.getElementById('closeModal');
 
 resumeBtn.addEventListener('click', () => {
-  resumeModal.style.display = 'flex';
+  resumeModal.classList.add("active");
+  resumeModal.classList.remove("active");
 });
 
 closeModal.addEventListener('click', () => {
@@ -23,4 +24,35 @@ window.addEventListener('click', (e) => {
   if (e.target === resumeModal) {
     resumeModal.style.display = 'none';
   }
+});
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if(sectionTop < window.innerHeight - 100) {
+      section.classList.add("show");
+    }
+  });
+});
+
+const navLinks = document.querySelectorAll('.navbar a');
+
+window.addEventListener('scroll', () => {
+  let current = "";
+
+  document.querySelectorAll("section").forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 150) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if(link.getAttribute("href") === `#${current}`){
+      link.classList.add("active");
+    }
+  });
 });
